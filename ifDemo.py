f19262f64f52a02a96e396a6a862197b08433fb1
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-29 08:12:43 -0500

    Create ifDemo.py

---
+++ ifDemo.py
@@ -0,0 +1,13 @@
+#Matt Westelman
+#ifDemo - how to use if, elif, else
+#1/29/18
+
+num=float(input("enter a number "))
+
+if num>0:
+    print(num, "is positive")
+elif num < 0:
+    print(num, "is negative")
+else:
+    print(num, "is zero")
+    
