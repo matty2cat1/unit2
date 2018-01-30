ec671b7c0e13b12d47687f5975e406cc30e16c43
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-30 08:19:15 -0500

    Update compoundDemo.py

--- compoundDemo.py
+++ compoundDemo.py
@@ -1,1 +1,16 @@
+#Matt Westelman
+#compoundDemo - compound if statements
+#1/30/18
 
+num = int(input("enter a number: "))
+
+if num > 0 and num%7 == 0:
+    print(num, "Is positive and divisible by 7")
+elif num > 0:
+    print(num, "Is positive and not divisible by 7")
+elif num < 0 and num%7 == 0:
+    print(num, "Is negative and divisible by 7")
+elif num < 0 and num%7 != 0:
+     print(num, "Is negative and divisible by 7")
+else:
+    print(num, "is zero")
