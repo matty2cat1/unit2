9b9e07c042607b8f9ec34caf52eec992c701a01d
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-31 08:39:19 -0500

    Create insulter.py

---
+++ insulter.py
@@ -0,0 +1,19 @@
+#Matt Westelman
+#insulter.py - being a general jerk
+#1/31/18
+
+from random import randint
+num = randint(1, 5)
+if num==1:
+    input("How much do you weigh?")
+    print("Hey TONS of fun! How bout you eat a salad?")
+elif num==2:
+    print("Please stop looking at the camera, you might shatter the lens")
+elif num==3:
+    height = input("How tall are you?")
+    print(height, "! I had no idea they piled crap that high!")
+elif num==4 
+    age = input("How old are you?")
+    print("That old? I would avoid antique stores if I were you. They might want to keep you!")
+else:
+    print("You are alright. I guess.")
