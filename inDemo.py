929ee0345ecfdc9c4efd4ea9854e12cc45e7c95d
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-31 08:07:32 -0500

    Create inDemo.py

---
+++ inDemo.py
@@ -0,0 +1,9 @@
+#Matt Westelman
+#inDemo.py - Conditionals and Strings
+#1/31/18
+
+word = input("enter a word ")
+if 'w' in word or "W" in word:
+    print(word, "is cool")
+else:
+    print(word, "is NOT cool")
