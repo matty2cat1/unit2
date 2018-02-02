b4c4230747f7300a3e4c8070671d8403a8a5c9ba
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-02 08:49:00 -0500

    Create brogue.py

---
+++ brogue.py
@@ -0,0 +1,20 @@
+#Matt Westelman
+#brogue
+#2/2/18
+
+print("You open the door, and see an axe and a rapier")
+weapon = input("Which do you pick up? (axe or rapier)")
+if weapon = "axe":
+    axe equip = input("equip the axe? (yes or no)")
+    if axe equip = "yes":
+        print("You can barely lift its massive weight: 2 more strength would be ideal")
+    else:
+        ("you put the axe in your pack")
+elif weapon = "rapier": 
+    sword equip = input("equip the rapier? (yes or no)")
+    if sword equip = "yes":
+        print("Your grip involontarily tightens around the sword. It must've been cursed!")
+    else:
+        print("you put the rapier in your pack")
+else: 
+    print("You took to long to think! A kobold attacks you.)
